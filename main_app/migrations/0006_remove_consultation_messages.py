e9c29f3e356d4aaef85d99f8cf443137ca65aed3
our_GitHub_user_name <our_GitHub_user_email>
2023-08-01 17:17:36 +0300

    first commit

---
+++ main_app/migrations/0006_remove_consultation_messages.py
@@ -0,0 +1,16 @@
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main_app', '0005_doctor_rating'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='consultation',
+            name='messages',
+        ),
+    ]
